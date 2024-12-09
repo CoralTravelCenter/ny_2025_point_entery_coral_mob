d0a6929b15d2dc191640a2e59330c9a53c3ecc70
Mike <mikhail-gankin@yandex.ru>
2024-12-09 10:52:22 +0300

    +++ hostReactReady

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import {notAuth} from "./not-auth/not-auth.js";
 import {auth} from "./auth/auth.js";
-import {appendOnce} from "./usefuls.js";
+import {appendOnce, hostReactAppReady} from "./usefuls.js";
 import Cookies from 'js-cookie'
 import MicroModal from 'micromodal';
 import './not-auth/not-auth.scss'
@@ -15,10 +15,13 @@ const modalId = 'sled-overlay';
 root.classList.add('ny_notauth_colba')
 MicroModal.init();
 
-if (Cookies.get('next-auth.session-token')) {
-	appendOnce(placeToInsert, auth(root, modalId, gamePage));
-	console.log('auth')
-} else {
-	appendOnce(placeToInsert, notAuth(root, modalId, gamePage));
-	console.log('not-auth')
-}
+hostReactAppReady().then(() => {
+	if (location.origin.includes('backoffice')) return
+	if (Cookies.get('next-auth.session-token')) {
+		appendOnce(placeToInsert, auth(root, modalId, gamePage));
+		console.log('auth')
+	} else {
+		appendOnce(placeToInsert, notAuth(root, modalId, gamePage));
+		console.log('not-auth')
+	}
+})
--- src/usefuls.js
+++ src/usefuls.js
@@ -3,3 +3,17 @@ export function appendOnce(parent, child) {
 		parent.append(child);
 	}
 }
+
+export async function hostReactAppReady(selector = "#__next > div", timeout = 500) {
+	return new Promise((resolve) => {
+		const waiter = () => {
+			const host_el = document.querySelector(selector);
+			if (host_el?.getBoundingClientRect().height) {
+				resolve();
+			} else {
+				setTimeout(waiter, timeout);
+			}
+		};
+		waiter();
+	});
+}
